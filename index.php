4921c1c649bf83cad512b23322bde97e74a95397
kmarukur <91945334+kmarukur@users.noreply.github.com>
2021-10-28 07:38:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <h5><b>Below image is from cloudfront</b></h5>
 <?php
    $firstline=`head -n1 path.txt`;          
-   $path_img="https://".$firstline."/coffee1.jpg";
+   $path_img="https://".$firstline."/coffee2.jpg";
    echo "<br>";
    echo "<img src='{$path_img}' width=500 height=500>";
 ?>
